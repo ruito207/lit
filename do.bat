1d24f69bf050f12be13f3803f2ad2e48de90e68d
ruito207 <84075322+ruito207@users.noreply.github.com>
2023-01-06 20:54:31 +0900

    Update do.bat

--- do.bat
+++ do.bat
@@ -1,10 +1,10 @@
 @echo off
 @echo SYSTEM IS STARTING...
 @echo POWERED BY RUITO
-@start https://player.lifeistech-lesson.jp/player/step?project_name=information1^&scenario_path=lesson/con_01_v2^&step_id=471
-@start https://player.lifeistech-lesson.jp/player/step?project_name=information1^&scenario_path=lesson/con_02_v2^&step_id=653
-@start https://player.lifeistech-lesson.jp/player/step?project_name=information1^&scenario_path=lesson/con_03_v2^&step_id=468
-@start https://player.lifeistech-lesson.jp/player/step?project_name=information1^&scenario_path=lesson/con_04_v2^&step_id=607
-@start https://player.lifeistech-lesson.jp/player/step?project_name=information1^&scenario_path=lesson/con_05_v2^&step_id=470
+@start https://player.lifeistech-lesson.jp/player/step?project_name=information1^&scenario_path=lesson/con_01_v2^&step_id=472
+@start https://player.lifeistech-lesson.jp/player/step?project_name=information1^&scenario_path=lesson/con_02_v2^&step_id=654
+@start https://player.lifeistech-lesson.jp/player/step?project_name=information1^&scenario_path=lesson/con_03_v2^&step_id=469
+@start https://player.lifeistech-lesson.jp/player/step?project_name=information1^&scenario_path=lesson/con_04_v2^&step_id=608
+@start https://player.lifeistech-lesson.jp/player/step?project_name=information1^&scenario_path=lesson/con_05_v2^&step_id=471
 @echo THE PROCESS WAS FINISHED
 @timeout 500
